52550e2f08939ac24d7f8a59962f22cdd0d376fb
ManavSaini124 <itsmemanavsaini@gmail.com>
2025-07-20 22:18:44 +0530

    Agent Data Table added

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -7,9 +7,16 @@ import { AgentsListHeader } from "@/app/modules/agents/ui/components/list-header
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { SearchParams } from "nuqs";
+import { loadSearchParams } from "@/app/modules/agents/params";
+
+interface props{
+    searchParams: Promise<SearchParams>
+}
 
 
-const page= async ()=>{
+const page= async ({searchParams}:props)=>{
+    const filters = await loadSearchParams(searchParams);
     const session = await auth.api.getSession({
         headers: await headers()
       });
@@ -18,7 +25,9 @@ const page= async ()=>{
         redirect("/auth/sign-in");
       }
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+        ...filters,
+    }));
 
     return (
         <>
--- src/app/modules/agents/ui/views/agents-view.tsx
+++ src/app/modules/agents/ui/views/agents-view.tsx
@@ -4,17 +4,36 @@ import {  useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading_state";
 import { ErrorState } from "@/components/error-state";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
+import { useAgentsFilter } from "../../hooks/use-agents-filter";
+import { DataPagination } from "../components/data-pagination";
+
 
 
 export const AgentsView = () => {
     const trpc = useTRPC();
-    const {data} =useSuspenseQuery(trpc.agents.getMany.queryOptions())
-
+    const [filters, setFilters] = useAgentsFilter()
     
+    const {data} =useSuspenseQuery(trpc.agents.getMany.queryOptions({
+        ...filters
+    }))
 
     return(
-        <div>
-            {JSON.stringify(data ,null ,2)}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+            <DataTable columns={columns} data={data.items} />
+            <DataPagination
+                page={filters.page}
+                totalPages={data.totalPages}
+                onPageChange={(page) => setFilters({ page })}
+            />
+            {data.items.length === 0 &&(
+                <EmptyState
+                    title="Create your first agent"
+                    description="Create a new agent to get started. Each Agent will folow your instruction and would intract with the participants based on instructions given"
+                />
+            )}
         </div>
     );
 };
